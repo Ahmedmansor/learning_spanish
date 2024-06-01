017e1bf380f40e9700f6e61868b5a162b651fa02
Ahmedmansor <conantotta@gmail.com>
2024-06-01 15:33:38 +0300

    Refactor UI and add new features
    - Updated the app theme to have a new color scheme and modified the navigation bar to have a new design.
    - Added a swap button to the translation screen to switch between English and Spanish languages.
    - Created a custom row widget to display language options and added a clear button to the translation text field.
    - Added a copy to clipboard feature to the translation screen.
    - Imported new packages for toast messages and text editing controllers.
    - Made minor changes to the discover screen's background color.

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -2,20 +2,31 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData.light(useMaterial3: true).copyWith(
-      appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xff000103),
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          )));
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    // scaffoldBackgroundColor: Colors.white,
+    navigationBarTheme: const NavigationBarThemeData(
+        indicatorColor: Colors.blue, backgroundColor: Colors.red),
+  );
 
   static ThemeData darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
-      appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xff000103),
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          )));
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.black,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        )),
+    // scaffoldBackgroundColor: Colors.black,
+    navigationBarTheme: const NavigationBarThemeData(
+      indicatorColor: Colors.red,
+      backgroundColor: Colors.red,
+    ),
+  );
 }
--- lib/cubits/translation/cubit/translation_cubit.dart
+++ lib/cubits/translation/cubit/translation_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:meta/meta.dart';
 import 'package:translator/translator.dart';
 
@@ -10,10 +13,45 @@ class TranslationCubit extends Cubit<TranslationState> {
 
   static TranslationCubit get(context) => BlocProvider.of(context);
 
-  String translated = 'Traduccion';
-  void translate(String text) async {
-    final translation = await text.translate(from: 'en', to: 'es');
-    translated = translation.text;
+  String from = 'en';
+  String to = 'es';
+  void swapLanguages() {
+    from != 'en' ? from = 'en' : from = 'es';
+    to != 'es' ? to = 'es' : to = 'en';
+    emit(SwapTranslationSucess());
+  }
+
+// Created a text editing controller to con clear the text in text field
+  TextEditingController textEditingController = TextEditingController();
+  String translated = '';
+
+  void translate(String text, String from, String to) async {
+    final translation = await text.translate(from: from, to: to);
     emit(TranslationSucess());
+    text = textEditingController.text;
+    translated = translation.text;
+  }
+
+  void clear() {
+    textEditingController.clear();
+    translated = '';
+    emit(ClearSucess());
+  }
+
+  Future copyToClipBoard() async {
+    await Clipboard.setData(ClipboardData(
+      text: translated,
+    ));
+
+    emit(copyToClipBoardSucess());
+    await Fluttertoast.showToast(
+        msg: "copied to clipboard",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        textColor: Colors.black,
+        backgroundColor: Colors.white,
+        fontSize: 16.0);
+    debugPrint('toast');
   }
 }
